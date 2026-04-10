80601d95f4e7a4408ce7a9b8598ae135f322c72c
KiveAllen <1348438509@qq.com>
2026-04-10 14:01:59 +0800

    chore(project): 初始化项目基础配置文件
    - 添加浏览器兼容性配置文件 .browserslistrc

---
+++ frontend/packages/@core/ui-kit/shadcn-ui/src/ui/alert-dialog/AlertDialogDescription.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import type {AlertDialogDescriptionProps} from 'reka-ui';
+import {AlertDialogDescription, useForwardProps} from 'reka-ui';
+
+import {computed} from 'vue';
+
+import {cn} from '@vben-core/shared/utils';
+
+const props = defineProps<AlertDialogDescriptionProps & { class?: any }>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <AlertDialogDescription
+    v-bind="forwardedProps"
+    :class="cn('text-muted-foreground text-sm', props.class)"
+  >
+    <slot></slot>
+  </AlertDialogDescription>
+</template>
---
+++ frontend/packages/@core/ui-kit/shadcn-ui/src/ui/badge/Badge.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type {BadgeVariants} from './badge';
+import {badgeVariants} from './badge';
+
+import {cn} from '@vben-core/shared/utils';
+
+const props = defineProps<{
+  class?: any;
+  variant?: BadgeVariants['variant'];
+}>();
+</script>
+
+<template>
+  <div :class="cn(badgeVariants({ variant }), props.class)">
+    <slot></slot>
+  </div>
+</template>
